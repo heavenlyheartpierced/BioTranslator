9ea66d761fec552232e8e7a37ffe952bf723bb1c
heavenlyheartpierced <westa6366@gmail.com>
2026-05-24 11:52:37 -0800

    Added CodeTester file, finished modifications on ProgramThree

--- CodeTester.java
+++ CodeTester.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProgramThree {
+public class CodeTester {
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
 
